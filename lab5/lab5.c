722d688612a9e77490d238f9d2b9cbd2929f6c5a
Maksym Andreiev <maxoggyha@gmail.com>
2021-12-17 01:07:46 +0200

    Update lab5.c
    Lots of changes

--- lab5/lab5.c
+++ lab5/lab5.c
@@ -1,51 +1,59 @@
 #include <stdio.h>
 #include <malloc.h>
 #include <time.h>
+#include <math.h>
 
 int main() {
     clock_t start, end;
     long double time;
     start = clock();
-    for (int x = 0; x <= 64; ++x) {
-        (int *) malloc(2 ^ x);
+    int x;
+    for (x = 0; x <= 64; ++x) {
+        (int *) malloc(pow(2, x));
     }
     end = clock();
     time = ((long double)(end - start))/ (CLOCKS_PER_SEC / 1000);
-    printf("Time of malloc() of 2 ^ x (in milliseconds): %Lf\n", time);
+    printf("Time of malloc() of 2 ^ x (in milliseconds): %Lf, x up to %d\n", time, x-1);
+    printf("x: %d", x);
     start = clock();
-    for (int x = 0; x <= 64; ++x) {
-        (int *) malloc(2 ^ (x + 1));
+    for (x = 0; x <= 64; ++x) {
+        (int *) malloc(pow(2, (x + 1)));
     }
     end = clock();
     time = ((long double)(end - start))/ (CLOCKS_PER_SEC / 1000);
-    printf("Time of malloc() of 2 ^ (x + 1) (in milliseconds): %Lf\n", time);
+    printf("Time of malloc() of 2 ^ (x + 1) (in milliseconds): %Lf, x up to %d\n", time, x-1);
     start = clock();
-    for (int x = 0; x <= 64; ++x) {
-        (int *) calloc(2 ^ x, sizeof(int));
+    for (x = 0; x <= 64; ++x) {
+        (int *) calloc(pow(2, x), sizeof(int));
     }
     end = clock();
     time = ((long double)(end - start))/ (CLOCKS_PER_SEC / 1000);
-    printf("Time of calloc() of 2 ^ x (in milliseconds): %Lf\n", time);
+    printf("Time of calloc() of 2 ^ x (in milliseconds): %Lf, x up to %d\n", time, x-1);
     start = clock();
-    for (int x = 0; x <= 64; ++x) {
-        (int *) calloc(2 ^ (x + 1), sizeof(int));
+    for (x = 0; x <= 64; ++x) {
+        (int *) calloc(pow(2, (x + 1)), sizeof(int));
     }
     end = clock();
     time = ((long double)(end - start))/ (CLOCKS_PER_SEC / 1000);
-    printf("Time of calloc() of 2 ^ (x + 1) (in milliseconds): %Lf\n", time);
+    printf("Time of calloc() of 2 ^ (x + 1) (in milliseconds): %Lf, x up to %d\n", time, x-1);
     start = clock();
-    for (int x = 0; x <= 64; ++x) {
-        (int *) alloca(sizeof(int) * (2 ^ x));
+    for (x = 0; x <= 64; ++x) {
+        (int *) alloca(sizeof(int) * (pow(2, x)));
+        if (x%2== 0){
+            printf("x: %d\t", x);
+        } else {
+            printf("x: %d\n", x);
+        }
     }
     end = clock();
     time = ((long double)(end - start))/ (CLOCKS_PER_SEC / 1000);
     printf("Time of alloca() of 2 ^ x (in milliseconds): %Lf\n", time);
     start = clock();
-    for (int x = 0; x <= 64; ++x) {
-        (int *) alloca(sizeof(int) * (2 ^ (x + 1)));
+    for (x = 0; x <= 64; ++x) {
+        (int *) alloca(sizeof(int) * (pow(2, (x + 1))));
     }
     end = clock();
     time = ((long double)(end - start))/ (CLOCKS_PER_SEC / 1000);
-    printf("Time of alloca() of 2 ^ (x + 1) (in milliseconds): %Lf\n", time);
+    printf("Time of alloca() of 2 ^ (x + 1) (in milliseconds): %Lf, x up to %d\n", time, x-1);
     return 0;
 }
